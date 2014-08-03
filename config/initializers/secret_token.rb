68cd07a3370e432fca6a25d8e2a08503b6a35aef
Tim Cook <tim@currinda.com>
2014-08-03 12:34:11 +1000

    Initialised kramer-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KramerRails::Application.config.secret_key_base = 'ba5f4bf9bfdea994d867ad3cf17cf7154c8532616d5198507a0680eaade430151b7714b3085a913309e70f5eb7e2096a9a0c1571e92b546be413111c760b3962'
