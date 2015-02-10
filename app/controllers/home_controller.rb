efb4f250f51e3bf6de792c2d7697eaf50b66ec4c
Tim Cook <tim@currinda.com>
2015-02-10 20:39:04 +1100

    update user model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @user = User.first
+    @items = Item.all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+
+  def name
+    first_name+" "+last_name
+  end
 end
